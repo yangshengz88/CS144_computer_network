53ee853e54dbce07e988beb6e584c6724a39ef04
yangshengz88 <83741306+yangshengz88@users.noreply.github.com>
2024-03-18 15:18:04 -0400

    implement lab2 part1

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return WrappingInt32{isn + static_cast<uint32_t>(n)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,20 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    
+    // the datatype of (n - isn) is int32. When int32 was coverted to uint32_t, uint_max + 1 was added implicitly
+    // see the link for more detail: https://stackoverflow.com/questions/4975340/int-to-unsigned-int-conversion
+    uint32_t offset = n - isn;
+
+    if (checkpoint > offset){
+        // add half of 2^32 in order to round up or round down for the distance between checkpoint and offset
+        uint64_t temp = (checkpoint - offset) + (1ul << 31);
+        // calculate how many wraps
+        uint64_t wrapNum = temp / (1ul << 32);
+        return wrapNum * (1ul << 32) + offset;
+    }
+    else {
+        return offset;
+    }
+
 }
